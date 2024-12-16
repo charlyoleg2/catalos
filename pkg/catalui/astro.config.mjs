4ed7844693c52cbbcb95d1f28ec5d886d738b22f
charlyoleg <charles.braquet@gmx.fr>
2024-12-16 22:25:28 +0100

    fix for github page

--- pkg/catalui/astro.config.mjs
+++ pkg/catalui/astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+//import node from "@astrojs/node";
+
+const basePath = process.env.BASE_PATH;
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	base: basePath ? basePath : '/',
+	output: 'static',
+	//output: "server",
+	//adapter: node({
+	//	mode: "standalone",
+	//}),
+});
