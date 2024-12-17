c21ec59de0eb0b2fd8a53a52ce64fcba2a523b65
charlyoleg <charles.braquet@gmx.fr>
2024-12-17 21:54:52 +0100

    second collection

--- pkg/catalui/src/content.config.ts
+++ pkg/catalui/src/content.config.ts
@@ -30,4 +30,61 @@ const users = defineCollection({
 	}),
 });
 
-export const collections = { users };
+const designs = defineCollection({
+	loader: glob({ pattern: '**/*.yaml', base: './collections/designs' }),
+	schema: z.object({
+		designId: z.number(),
+		designName: z.string(),
+		description: z.string(),
+		owner: z.string(),
+		createdAt: z.coerce.date(),
+		updatedAt: z.coerce.date(),
+		updateCount: z.number(),
+		visitedAt: z.coerce.date(),
+		visitCount: z.number(),
+		deletedAt: z.coerce.date(),
+		visible: z.boolean(),
+		likeCount: z.number(),
+		likeLastUsers: z.string().array().max(3),
+		linkToUi: z.string().url(),
+		linkToSrc: z.string().url(),
+		linkToPkg: z.string().url(),
+		linkToCli: z.string().url(),
+		linkToUis: z.string().url(),
+		linkToRepo: z.string().url(),
+		linkOthers: z.string().url().array(),
+		files: z.array(
+			z.object({
+				fileName: z.string(),
+				fileType: z.enum([
+					'png',
+					'jpg',
+					'pxJson',
+					'paxJson',
+					'dxf',
+					'svg',
+					'stl',
+					'brep',
+					'step',
+					'glb',
+					'3mf',
+					'txtLog',
+					'scad',
+					'jsCad',
+					'jsManifold',
+					'pyFreecad',
+				]),
+				filePath: z.string(),
+				fileSize: z.number(),
+				createdAt: z.coerce.date(),
+				updatedAt: z.coerce.date(),
+				updateCount: z.number(),
+				downloadedAt: z.coerce.date(),
+				downloadCount: z.number(),
+				deletedAt: z.coerce.date(),
+			})
+		),
+	}),
+});
+
+export const collections = { users, designs };
