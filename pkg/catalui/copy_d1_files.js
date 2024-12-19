8f66e4fadcdaf636ca488f27b9706d92e7e2f112
charlyoleg <charles.braquet@gmx.fr>
2024-12-19 22:56:41 +0100

    copy assets to public

---
+++ pkg/catalui/copy_d1_files.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+// copy_d1_files.js
+import fs from 'fs-extra';
+
+async function copyFiles() {
+	try {
+		await fs.copy('./d1/users/charlyoleg.svg', './public/u/charlyoleg.svg');
+	} catch(err) {
+		console.log('ERR543: Error while copying files to public');
+		console.log(err);
+	}
+}
+
+copyFiles();
