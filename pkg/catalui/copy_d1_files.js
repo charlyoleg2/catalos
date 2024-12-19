1e62534d7bb4f8c74be0baf3c64be149ff186184
charlyoleg <charles.braquet@gmx.fr>
2024-12-19 23:04:49 +0100

    starting copy script

--- pkg/catalui/copy_d1_files.js
+++ pkg/catalui/copy_d1_files.js
@@ -5,6 +5,7 @@ import fs from 'fs-extra';
 async function copyFiles() {
 	try {
 		await fs.copy('./d1/users/charlyoleg.svg', './public/u/charlyoleg.svg');
+		await fs.copy('./d1/users/carlo78.svg', './public/u/carlo78.svg');
 	} catch (err) {
 		console.log('ERR543: Error while copying files to public');
 		console.log(err);
