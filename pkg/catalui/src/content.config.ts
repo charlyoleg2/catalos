cde5596182b4fabe50c5b5fdb701b70764e6de88
charlyoleg <charles.braquet@gmx.fr>
2024-12-19 21:42:06 +0100

    reference wip

--- pkg/catalui/src/content.config.ts
+++ pkg/catalui/src/content.config.ts
@@ -1,43 +1,42 @@
 // src/content.config.ts
 
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, reference, z } from 'astro:content';
 //import { glob, file } from 'astro/loaders';
 import { glob } from 'astro/loaders';
 
 const users = defineCollection({
-	loader: glob({ pattern: '*.yaml', base: './collections/users' }),
-	schema: ({ image }) =>
-		z.object({
-			userId: z.number(),
-			username: z.string(),
-			firstName: z.string(),
-			familyName: z.string(),
-			birthDate: z.date(),
-			country: z.string(),
-			city: z.string(),
-			photoPath: image(),
-			email: z.string().email(),
-			passwd: z.coerce.string(),
-			createdAt: z.coerce.date(),
-			verifiedAt: z.coerce.date(),
-			updatedAt: z.coerce.date(),
-			updateCount: z.number(),
-			lastLoginAt: z.coerce.date(),
-			lastIP: z.string().ip(),
-			loginCount: z.number(),
-			deletedUserAt: z.coerce.date(),
-			deletedAdminAt: z.coerce.date(),
-			admin: z.boolean(),
-		}),
+	loader: glob({ pattern: '*.yaml', base: './d1/users' }),
+	schema: z.object({
+		userId: z.number(),
+		username: z.string(),
+		firstName: z.string(),
+		familyName: z.string(),
+		birthDate: z.date(),
+		country: z.string(),
+		city: z.string(),
+		photoPath: z.string(),
+		email: z.string().email(),
+		passwd: z.coerce.string(),
+		createdAt: z.coerce.date(),
+		verifiedAt: z.coerce.date(),
+		updatedAt: z.coerce.date(),
+		updateCount: z.number(),
+		lastLoginAt: z.coerce.date(),
+		lastIP: z.string().ip(),
+		loginCount: z.number(),
+		deletedUserAt: z.coerce.date(),
+		deletedAdminAt: z.coerce.date(),
+		admin: z.boolean(),
+	}),
 });
 
 const designs = defineCollection({
-	loader: glob({ pattern: '**/*.yaml', base: './collections/designs' }),
+	loader: glob({ pattern: '**/*.yaml', base: './d1/designs' }),
 	schema: z.object({
 		designId: z.number(),
 		designName: z.string(),
 		description: z.string(),
-		owner: z.string(),
+		owner: reference('users'),
 		createdAt: z.coerce.date(),
 		updatedAt: z.coerce.date(),
 		updateCount: z.number(),
