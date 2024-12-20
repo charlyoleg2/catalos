5020bfd5c4fe22825b5b644b4f842faca26092ff
charlyoleg <charles.braquet@gmx.fr>
2024-12-20 22:00:02 +0100

    content.config.js seems not being able to import modules

--- pkg/catalui/src/content.config.ts
+++ pkg/catalui/src/content.config.ts
@@ -3,7 +3,6 @@
 import { defineCollection, reference, z } from 'astro:content';
 //import { glob, file } from 'astro/loaders';
 import { glob } from 'astro/loaders';
-//import { schemaDesign } from './lib/util.ts';
 
 const users = defineCollection({
 	loader: glob({ pattern: '*.yaml', base: './d1/users' }),
@@ -31,64 +30,61 @@ const users = defineCollection({
 	}),
 });
 
-const listSuffix = [
-	'png',
-	'jpg',
-	'pxJson',
-	'paxJson',
-	'dxf',
-	'svg',
-	'stl',
-	'brep',
-	'step',
-	'glb',
-	'3mf',
-	'txtLog',
-	'scad',
-	'jsCad',
-	'jsManifold',
-	'pyFreecad',
-];
-
-const schemaDesign = z.object({
-	designId: z.number(),
-	designName: z.string(),
-	description: z.string(),
-	owner: reference('users'),
-	createdAt: z.coerce.date(),
-	updatedAt: z.coerce.date(),
-	updateCount: z.number(),
-	visitedAt: z.coerce.date(),
-	visitCount: z.number(),
-	deletedAt: z.coerce.date(),
-	visible: z.boolean(),
-	likeCount: z.number(),
-	likeLastUsers: z.string().array().max(3),
-	linkToUi: z.string().url(),
-	linkToSrc: z.string().url(),
-	linkToPkg: z.string().url(),
-	linkToCli: z.string().url(),
-	linkToUis: z.string().url(),
-	linkToRepo: z.string().url(),
-	linkOthers: z.string().url().array(),
-	files: z.array(
-		z.object({
-			fileName: z.string(),
-			fileType: z.enum(listSuffix),
-			filePath: z.string(),
-			fileSize: z.number(),
-			createdAt: z.coerce.date(),
-			updatedAt: z.coerce.date(),
-			updateCount: z.number(),
-			downloadedAt: z.coerce.date(),
-			downloadCount: z.number(),
-			deletedAt: z.coerce.date(),
-		})
-	),
-});
 const designs = defineCollection({
 	loader: glob({ pattern: '**/*.yaml', base: './d1/designs' }),
-	schema: schemaDesign,
+	schema: z.object({
+		designId: z.number(),
+		designName: z.string(),
+		description: z.string(),
+		owner: reference('users'),
+		createdAt: z.coerce.date(),
+		updatedAt: z.coerce.date(),
+		updateCount: z.number(),
+		visitedAt: z.coerce.date(),
+		visitCount: z.number(),
+		deletedAt: z.coerce.date(),
+		visible: z.boolean(),
+		likeCount: z.number(),
+		likeLastUsers: z.string().array().max(3),
+		linkToUi: z.string().url(),
+		linkToSrc: z.string().url(),
+		linkToPkg: z.string().url(),
+		linkToCli: z.string().url(),
+		linkToUis: z.string().url(),
+		linkToRepo: z.string().url(),
+		linkOthers: z.string().url().array(),
+		files: z.array(
+			z.object({
+				fileName: z.string(),
+				fileType: z.enum([
+					'png',
+					'jpg',
+					'pxJson',
+					'paxJson',
+					'dxf',
+					'svg',
+					'stl',
+					'brep',
+					'step',
+					'glb',
+					'3mf',
+					'txtLog',
+					'scad',
+					'jsCad',
+					'jsManifold',
+					'pyFreecad',
+				]),
+				filePath: z.string(),
+				fileSize: z.number(),
+				createdAt: z.coerce.date(),
+				updatedAt: z.coerce.date(),
+				updateCount: z.number(),
+				downloadedAt: z.coerce.date(),
+				downloadCount: z.number(),
+				deletedAt: z.coerce.date(),
+			})
+		),
+	}),
 });
 
 export const collections = { users, designs };
