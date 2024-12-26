d272f283214ad8d02d653edb7f5cdb7e3124cc88
charlyoleg <charles.braquet@gmx.fr>
2024-12-26 21:31:45 +0100

    generate path for all designs

--- pkg/catalui/src/lib/util.ts
+++ pkg/catalui/src/lib/util.ts
@@ -1,5 +1,10 @@
 // src/lib/util.ts
 
+interface tOneDesi {
+	user: string;
+	design: string;
+}
+
 function prefixBase(iPath: string): string {
 	const base = import.meta.env.BASE_URL;
 	const base2 = base === '/' ? '' : base;
@@ -12,4 +17,17 @@ function prunePrefix(iDesi: string): string {
 	return rDname;
 }
 
-export { prefixBase, prunePrefix };
+function splitDesiPath(iDesi: string): tOneDesi {
+	let rUser = 'oops';
+	let rDesign = 'oops';
+	const words = iDesi.split('/');
+	if (words.length === 2) {
+		rUser = words[0];
+		rDesign = words[1];
+	}
+	const rObj: tOneDesi = { user: rUser, design: rDesign };
+	return rObj;
+}
+
+export type { tOneDesi };
+export { prefixBase, prunePrefix, splitDesiPath };
