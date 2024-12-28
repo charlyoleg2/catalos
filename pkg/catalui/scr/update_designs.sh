1ed702bcb3a4a7a55c5c37859255bb409b44c219
charlyoleg <charles.braquet@gmx.fr>
2024-12-28 23:19:56 +0100

    3 designs of tuto-1

--- pkg/catalui/scr/update_designs.sh
+++ pkg/catalui/scr/update_designs.sh
@@ -3,5 +3,6 @@
 
 cd $(dirname $0)/..
 echo "update_designs.sh says Hello!"
-node scr/gen_desi.js --inDir ../../../cabane/refs --outDir d1/tutorial-1
+#node scr/gen_desi.js --inDir ../../../cabane/refs --outDir d1/tuto-1
+node scr/gen_desi.js --inDir ../../../ustensile/refs --outDir d1/tuto-1
 echo "update_designs.sh says Bye!"
