766cf0839260557e7a46826230905bece0176c37
charlyoleg <charles.braquet@gmx.fr>
2025-01-02 23:19:25 +0100

    using degit

--- pkg/catalui/scr/update_d1.sh
+++ pkg/catalui/scr/update_d1.sh
@@ -5,4 +5,7 @@ cd $(dirname $0)/..
 echo "update_d1.sh says Hello!"
 #node scr/update_db.js --inDir ../../../cabane/refs --outDir d1/designs/tuto-1
 node scr/update_db.js --inDir ../../../ustensile/refs --outDir d1/designs/tuto-1
+###
+npx degit https://github.com/charlyoleg2/gears_and_springs tmp/
+node scr/update_db.js --inDir tmp/gears_and_springs/refs --outDir d1/designs/tuto-1
 echo "update_d1.sh says Bye!"
