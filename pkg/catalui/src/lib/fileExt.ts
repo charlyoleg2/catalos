68a03aa3edb5458af63be9bf37d23c0d3393ee54
charlyoleg <charles.braquet@gmx.fr>
2025-01-07 21:59:24 +0100

    renaming extension in table

---
+++ pkg/catalui/src/lib/fileExt.ts
@@ -0,0 +1,34 @@
+// src/lib/fileExt.ts
+
+function renameExt(iExt: string): string {
+	let rExt = `9- ${iExt}`;
+	if (['pxJson'].includes(iExt)) {
+		rExt = `0-params- ${iExt}`;
+	} else if (['txtLog'].includes(iExt)) {
+		rExt = `1-log- ${iExt}`;
+	} else if (['svg', 'dxf'].includes(iExt)) {
+		rExt = `2d- ${iExt}`;
+	} else if (['step', 'stl', 'brep', 'glb', '3mf'].includes(iExt)) {
+		rExt = `3d- ${iExt}`;
+	} else if (['paxJson'].includes(iExt)) {
+		rExt = `4-pax- ${iExt}`;
+	} else if (['jsCad', 'jsManifold', 'scad', 'pyFreecad'].includes(iExt)) {
+		rExt = `5-script- ${iExt}`;
+	}
+	return rExt;
+}
+
+function selectFileTable(iExt: string): boolean {
+	const rSelect = iExt === 'image' ? false : true;
+	return rSelect;
+}
+
+function selectFileVignette(iExt: string): boolean {
+	let rSelect = false;
+	if (['image', 'svg'].includes(iExt)) {
+		rSelect = true;
+	}
+	return rSelect;
+}
+
+export { renameExt, selectFileTable, selectFileVignette };
