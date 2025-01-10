263b89c4598c4e4c318ba3a42ae7d723294b0ac1
charlyoleg <charles.braquet@gmx.fr>
2025-01-10 20:31:29 +0100

    dxf-viewer

--- pkg/catalui/src/lib/viewer-url.ts
+++ pkg/catalui/src/lib/viewer-url.ts
@@ -1,5 +1,6 @@
 // viewer-url.ts
 
+const urlDxfViewer = 'https://vagran.github.io/dxf-viewer-example/';
 const urlStepViewer = 'https://stepbro.vercel.app/';
 const urlStlViewer = 'https://stl-viewer.dualbox.com/';
 const urlGltfViewer = 'https://gltf-viewer.donmccurdy.com/';
@@ -7,4 +8,12 @@ const urlGlbViewer = 'https://app.web3d.be/';
 const url3dViewer = 'https://3dviewer.net/';
 const urlCadViewer = 'https://sharecad.org/';
 
-export { urlStepViewer, urlStlViewer, urlGltfViewer, urlGlbViewer, url3dViewer, urlCadViewer };
+export {
+	urlDxfViewer,
+	urlStepViewer,
+	urlStlViewer,
+	urlGltfViewer,
+	urlGlbViewer,
+	url3dViewer,
+	urlCadViewer,
+};
