6c2904542f1803c4207d55459be5404506e52f4e
charlyoleg <charles.braquet@gmx.fr>
2025-01-14 20:40:01 +0100

    d2 fetch heliostat_model_50cm

--- pkg/catalui/scr/update_d1.js
+++ pkg/catalui/scr/update_d1.js
@@ -47,6 +47,7 @@ if (eDBDIR === 'd2') {
 	console.log('update_d1.js says Hello with DBDIR=d2!');
 	await degit_and_update('https://github.com/charlyoleg2/gears_and_springs', 'charli78', vDBDIR);
 	await degit_and_update('https://github.com/charlyoleg2/ustensile', 'jack08', vDBDIR);
+	await degit_and_update('https://github.com/charlyoleg2/heliostat_model_50cm', 'ada', vDBDIR);
 	console.log('update_d1.js says Bye with DBDIR=d2!');
 } else {
 	const vDBDIR = 'd1';
